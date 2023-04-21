d31ba16bcac2fbc2e8a41ed08c3a97a8a96f0126
Daniel Haida <danilgajda22@gmail.com>
2023-04-21 14:02:30 +0300

    fixing WP & GP

--- src/GenericComponents/GenericButton/Button.js
+++ src/GenericComponents/GenericButton/Button.js
@@ -5,8 +5,10 @@ function Button(props) {
   const { onClick, children, title, disabled = false } = props
   return (
     <button
+
       {...props}
       className={styles.button}
+      style={styles}
       onClick={onClick}
       title={title}
       disabled={disabled}>
